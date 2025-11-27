93ad6d61dbc6c8ade5fe5922796f873237d87192
Pedro Pesantez Pacheco <ppesantez97@gmail.com>
2025-11-26 21:31:07 -0500

    Correcion de ruta archivos

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
-import "bootstrap";
-import "./style.css";
+function excuse(){
 
+    let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+    let action = ['ate', 'peed', 'crushed', 'broke'];
+    let what = ['my homework', 'my phone', 'the car'];
+    let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+    let indexWho = Math.floor(Math.random()*who.length)
+    let indexAction = Math.floor(Math.random()*action.length)
+    let indexWhat = Math.floor(Math.random()*what.length)
+    let indexWhen = Math.floor(Math.random()*when.length)
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    let phrase = "" 
+    phrase = who[indexWho] + " " + action[indexAction] + " " + what[indexWhat] + " " + when[indexWhen]
+    return phrase
+    
+}
+
+//Obtener la frase:
+const fraseAleatoria = excuse();
+
+//Obtener el elemento HTML por su ID:
+const contenedor = document.getElementById("excuseText");
+
+//Inyectar la frase:
+contenedor.textContent = fraseAleatoria;
